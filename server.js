e50e996986fb083a2f675334c8a1e60e3bbccdb2
praj3004 <prajaktadhamane10@gmail.com>
2024-10-13 02:15:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000; // Use PORT from environment or default to 3000
+const port = process.env.PORT || 4000; // Use PORT from environment or default to 3000
 
 // Middleware
 app.use(cors());
